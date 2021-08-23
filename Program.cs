5ca740eba92e10f9ca9a2c7dc20d7bc95efbf6a9
Pedrohme <pedrohmen45@gmail.com>
2021-08-23 16:19:44 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GFTSTART2_Etapa_Tecnica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
