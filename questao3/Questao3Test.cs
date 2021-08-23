3b61ece0be9e3824d11e6e49384291c226a625d6
Pedrohme <pedrohmen45@gmail.com>
2021-08-23 17:51:19 -0300

    docs: comentarios no arquivo do teste

--- questao3/Questao3Test.cs
+++ questao3/Questao3Test.cs
@@ -3,12 +3,13 @@ namespace GFTSTART2_Etapa_Tecnica.questao3 {
         public void RunTest() {
             Entregador correio = new Entregador();
             Morador[] moradores = new Morador[] {
-                new Morador("15987654321", 200, true),
-                new Morador("15941241323", 311, false),
-                new Morador("15945341675", 542, false),
-                new Morador("15968482384", 697, true),
-                new Morador("15955433424", 753, false)
+                new Morador("15987654321", 200, true), //casa par, possui cachorro = não entrega
+                new Morador("15941241323", 311, false),//casa impar, nao possui cachorro = entrega
+                new Morador("15945341675", 542, false),//casa par, nao possui cachorro = entrega
+                new Morador("15968482384", 697, true), //casa impar, possui cachorro = não entrega
+                new Morador("15955433424", 753, false) //casa impar, nao possui cachorro = não entrega
             };
+            //total: 3 casas entregues, sendo 2 impares
 
             foreach (var morador in moradores) {
                 correio.Entregar(morador);
